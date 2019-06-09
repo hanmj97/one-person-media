f9038306adc44f1e738a013fd541fd8547e558f5
hanmj97 <38197067+hanmj97@users.noreply.github.com>
2019-06-09 15:03:45 +0900

    Create geasipan2.java

---
+++ test1/Java Resources/src/geasipan2/geasipan2.java
@@ -0,0 +1,54 @@
+package geasipan2;
+
+public class geasipan2 {
+
+	private int number;
+	private String title;
+	private String userID;
+	private String userTel;
+	private String date;
+	private String content;
+	private int delete;
+	public int getnumber() {
+		return number;
+	}
+	public void setnumber(int number) {
+		this.number = number;
+	}
+	public String gettitle() {
+		return title;
+	}
+	public void settitle(String title) {
+		this.title = title;
+	}
+	public String getuserID() {
+		return userID;
+	}
+	public void setuserID(String userID) {
+		this.userID = userID;
+	}
+	public String getuserTel() {
+		return userTel;
+	}
+	public void setuserTel(String userTel) {
+		this.userTel = userTel;
+	}
+	public String getdate() {
+		return date;
+	}
+	public void setdate(String date) {
+		this.date = date;
+	}
+	public String getcontent() {
+		return content;
+	}
+	public void setcontent(String content) {
+		this.content = content;
+	}
+	public int getdelete() {
+		return delete;
+	}
+	public void setdelete(int delete) {
+		this.delete = delete;
+	}
+}
